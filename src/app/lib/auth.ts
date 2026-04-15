e5713c2fd8382eed42e9900913d85d88852e016a
usernayeem <93907431+usernayeem@users.noreply.github.com.>
2026-04-16 04:28:02 +0600

    feat: setup prisma client, better-auth, cloudinary, and stripe

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,148 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { bearer, emailOTP } from "better-auth/plugins";
+import { Role, UserStatus } from "../../generated/prisma/enums";
+import { envVars } from "../config/env";
+import { sendEmail } from "../utils/email";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+    baseURL: envVars.BETTER_AUTH_URL,
+    secret: envVars.BETTER_AUTH_SECRET,
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+
+    emailAndPassword: {
+        enabled: true,
+        requireEmailVerification: true,
+    },
+
+    socialProviders:{
+        google:{
+            clientId: envVars.GOOGLE_CLIENT_ID,
+            clientSecret: envVars.GOOGLE_CLIENT_SECRET,
+            mapProfileToUser: ()=>{
+                return {
+                    role: Role.STUDENT,
+                    status: UserStatus.ACTIVE,
+                    needPasswordChange: false,
+                    emailVerified: true,
+                    isDeleted: false,
+                    deletedAt: null,
+                }
+            }
+        }
+    },
+
+    emailVerification:{
+        sendOnSignUp: true,
+        sendOnSignIn: true,
+        autoSignInAfterVerification: true,
+    },
+
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                required: true,
+                defaultValue: Role.STUDENT
+            },
+
+            status: {
+                type: "string",
+                required: true,
+                defaultValue: UserStatus.ACTIVE
+            },
+
+            needPasswordChange: {
+                type: "boolean",
+                required: true,
+                defaultValue: false
+            },
+
+            isDeleted: {
+                type: "boolean",
+                required: true,
+                defaultValue: false
+            },
+
+            deletedAt: {
+                type: "date",
+                required: false,
+                defaultValue: null
+            },
+        }
+    },
+
+    plugins: [
+        bearer(),
+        emailOTP({
+            overrideDefaultEmailVerification: true,
+            async sendVerificationOTP({email, otp, type}) {
+                if(type === "email-verification"){
+                  const user = await prisma.user.findUnique({
+                    where : { email, }
+                  })
+
+                   if(!user){ return;}
+                   if(user && user.role === Role.SUPER_ADMIN){ return; }
+                  
+                    if (user && !user.emailVerified){
+                    sendEmail({
+                        to : email,
+                        subject : "Verify your email",
+                        templateName : "otp",
+                        templateData :{
+                            name : user.name,
+                            otp,
+                        }
+                    })
+                  }
+                }else if(type === "forget-password"){
+                    const user = await prisma.user.findUnique({
+                        where : { email, }
+                    })
+
+                    if(user){
+                        sendEmail({
+                            to : email,
+                            subject : "Password Reset OTP",
+                            templateName : "otp",
+                            templateData :{
+                                name : user.name,
+                                otp,
+                            }
+                        })
+                    }
+                }
+            },
+            expiresIn : 2 * 60, // 2 minutes in seconds
+            otpLength : 6,
+        })
+    ],
+
+    session: {
+        expiresIn: 60 * 60 * 60 * 24, // 1 day in seconds
+        updateAge: 60 * 60 * 60 * 24, // 1 day in seconds
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 60 * 24, // 1 day in seconds
+        }
+    },
+
+    redirectURLs:{
+        signIn : `${envVars.BETTER_AUTH_URL}/api/v1/auth/google/success`,
+    },
+
+    trustedOrigins: [process.env.BETTER_AUTH_URL || "http://localhost:5000", envVars.FRONTEND_URL],
+
+    advanced: {
+        useSecureCookies : false,
+        cookies:{
+            state:{ attributes:{ sameSite: "none", secure: true, httpOnly: true, path: "/", } },
+            sessionToken:{ attributes:{ sameSite: "none", secure: true, httpOnly: true, path: "/", } }
+        }
+    }
+
+});
---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
+import { envVars } from '../config/env';
+
+const connectionString = envVars.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma };
